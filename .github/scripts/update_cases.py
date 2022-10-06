b0b298d68b184570ea2eebd76c4aa2d738c3dafe
watronfire <snowboardman007@gmail.com>
2022-10-06 11:22:52 -0700

    Supply fake header

--- .github/scripts/update_cases.py
+++ .github/scripts/update_cases.py
@@ -1,13 +1,13 @@
 from tableauscraper import TableauScraper as TS
-import requests
+from subprocess import run
 from bs4 import BeautifulSoup
 import urllib3
 from datetime import datetime
 import pandas as pd
 
 init_url = "https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/human-monkeypox/localcases.html"
-r = requests.get( init_url )
-soup = BeautifulSoup( r.text, "html.parser" )
+request = run( f"curl -A 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' {init_url}", shell=True, capture_output=True, text=True )
+soup = BeautifulSoup( request.stdout, "html.parser" )
 iframe = soup.find( "div", {"class": "external parbase section"} ).find( "iframe" )
 iframe_src = iframe.attrs["src"]
 db_source = urllib3.util.parse_url( iframe_src ).path
@@ -26,7 +26,7 @@ case_df = pd.read_csv( "MPX_cases.csv", parse_dates=["date"])
 
 # if found date is greater than last date in cases database
 if case_df.iloc[-1]["date"] < date:
-    assert case_df.iloc[-1]["cases"] < cases, f"New cases ({cases}) is less than the previous data ({case_df.iloc[-1]['date']})"
+    assert case_df.iloc[-1]["cases"] <= cases, f"New cases ({cases}) on {date} is less than the previous data ({case_df.iloc[-1]['date'].strftime('%Y-%m-%d')}: {case_df.iloc[-1]['cases']})"
     print( "New data has been found" )
     case_df = pd.concat( [case_df, pd.DataFrame({"date" : date, "cases" : cases }, index=[1])], ignore_index=True )
     case_df.to_csv( "MPX_cases.csv", index=False )
